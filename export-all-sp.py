8c771a39d8d38afca74e2ddcc0199c0225c09858
toneillcodes <tom.oneill13@gmail.com>
2026-03-10 08:10:32 -0400

    Updating export script to paginate through applications when there are more than 100

--- export-all-sp.py
+++ export-all-sp.py
@@ -1,24 +1,36 @@
 import json
+import getpass
+import urllib3
 import requests
 from requests.auth import HTTPBasicAuth
-host = input("Carbon Console Hostname/Port: ")
 
-admin = input("Carbon Console Admin: ")
-password = input("Carbon Console Password: ")
+# prompt for hostname/port and credentials
+host = getpass.getpass("Carbon Console Hostname/Port: ").strip()
+admin = getpass.getpass("Carbon Console Admin: ").strip()
+password = getpass.getpass("Carbon Console Password: ").strip()
+
+# build authentication object from credentials
 basic = HTTPBasicAuth(admin, password)
 
+# prompt for the download directory
 download_dir = input("XML Download Directory: ")
 
-## try to retrieve the first 30 applications
+# initialize an empty app list
+all_applications = []
+
+# try to retrieve the first 30 applications
 print("Performing intial query")
+
+urllib3.disable_warnings()
+
 req_url = 'https://' + host + '/t/carbon.super/api/server/v1/applications'  
 app_check = requests.get(req_url, auth=basic, verify=False)
-print(app_check.status_code)
+#print(app_check.status_code)
 if(app_check.status_code == 200 or app_check.status_code == 201):
     #print(app_check.text)
-    ## if this condition is true, then we got the Carbon login page
+    ## if this condition is true, then we got the Carbon login page and it's likely the wrong URL
     if("WSO2 Management Console" in app_check.text):
-        print("Failed, Carbon Console login page detected, double check the URL and EEI > 5.10.0")
+        print("Failed, Carbon Console login page detected, double check the URL and EEI >= 5.10.0")
         print("URL: " + req_url)
     else:
         ## might have a valid response, let's try parsing the JSON
@@ -26,25 +38,44 @@ if(app_check.status_code == 200 or app_check.status_code == 201):
         #print(app_list_json)
 
         ## total number of applications
-        app_count = app_list_json['totalResults']
-        print(app_list_json['totalResults'])
+        app_count = app_list_json['totalResults']        
         if(app_count is None):
             print("Failed, unable to determine the app count")
         else:
-            print("Querying the full application list")
-            ## query the full list of applications using the totalResults from our first request as the limit
-            full_app_list = requests.get('https://' + host + '/t/carbon.super/api/server/v1/applications?limit=' + str(app_count), auth=basic, verify=False)
-            #print(full_app_list.text)
-            full_app_list_json = json.loads(full_app_list.text)
-            app_list = full_app_list_json['applications']
-            print(app_list)
-            for app in app_list:
-                print("Parsing " + app['name'])
+            #print("Count is greater than 100, running loop")
+            upper_bound = 100
+            # range(start, stop, step)
+            # This will give us offsets: 0, 100, 200, etc.
+            for lower_bound in range(0, app_count, upper_bound):
+                
+                url = f'https://{host}/t/carbon.super/api/server/v1/applications'
+                params = {
+                    'offset': lower_bound,
+                    'limit': upper_bound  # Limit stays 100 to get the next "chunk"
+                }
+                
+                response = requests.get(url, params=params, auth=basic, verify=False)
+                
+                if response.status_code == 200:
+                    apps = response.json()
+                    # Extract the list from the current page
+                    current_page_apps = apps.get('applications', [])
+                    
+                    # Add this page's items to the master list
+                    all_applications.extend(current_page_apps)
+                    
+                    print(f"Collected {len(all_applications)} / {app_count} apps...")
+                else:
+                    print(f"Error fetching data: {response.status_code}")
+                    exit
+
+            for app in all_applications:
+                #print("Parsing " + app['name'])
                 if(app['name'] == 'User Portal' or app['name'] == 'wso2carbon-local-sp'):
                     print("Skipping default app: " + app['name'])
                 else:
                     res_export = requests.get('https://' + host + '/t/carbon.super/api/server/v1/applications/' + app['id'] + '/export', auth=basic, verify=False)
-                    print(res_export.status_code)
+                    #print(res_export.status_code)
                     if(res_export.status_code == 200):
                         #print(res_export.text)
                         filename = download_dir + '\\' + app['name'] + '.xml'
@@ -52,8 +83,8 @@ if(app_check.status_code == 200 or app_check.status_code == 201):
                         f.write(res_export.text)
                         f.close()
                     else:
-                        print("Failed to export " + app['name'])
-            print("Export complete.")
+                        print("Failed to export " + app['name'])            
+        print("Export complete.")
 else:
     print("Failed to query app list, status code:")
     print(app_check.status_code)
